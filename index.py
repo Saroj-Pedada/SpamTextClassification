5556dd14c0981d8ca951042b1c8b9f8718fa1740
Saroj Pedada <77237839+Saroj-Pedada@users.noreply.github.com>
2023-04-20 01:31:18 +0530

    Update index.py

--- index.py
+++ index.py
@@ -40,12 +40,17 @@ st.header('Enter any string:')
 sample_texts = st.text_input('sample_texts')
 with open('tokenizer.pickle', 'rb') as handle:
     tok = pickle.load(handle)
-
+sample_texts = ["Free entry in 2 a wkly comp to win FA Cup final tkts 21st May 2005",
+                "i'm not coming to home today",
+                "ok. i'm good",
+                "congratulations you won! sms YES on 54233 to win",
+               "Your 2004 account for 07XXXXXXXXX shows 786"]
 if st.button('Predict Spam/Ham'):
     txts = tok.texts_to_sequences([sample_texts])
     txts = pad_sequences(txts, maxlen=max_len)
     preds = model.predict(txts)
-    if(preds[0]>0.5):
-        st.success(preds)
-    else:
-        st.success(preds)
+    for x in preds :
+        if(x<=0.5):
+            st.success("Ham")
+        else:
+            st.success("Spam")
